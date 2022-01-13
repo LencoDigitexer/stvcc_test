c302fc89ae35a70b09b9cad8743ba15cdb5e8d85
Илья Евсеенко <addminuser258@gmail.com>
2022-01-13 22:31:22 +0300

    обновил кодировку

--- content.js
+++ content.js
@@ -1,3 +1,19 @@
+function getText(){
+    // read text from URL location
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://lencodigitexer.github.io/stvcc_test/0212z02.txt', true);
+    request.send(null);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            var type = request.getResponseHeader('Content-Type');
+            if (type.indexOf("text") !== 1) {
+                return request.responseText;
+            }
+        }
+    }
+}
+
+
 console.log("test");
 
 console.log(document.querySelector("body > form > table > tbody > tr:nth-child(3) > td:nth-child(2)").textContent);
